c5334cc56bbf8c6e8eb9b1b47ead800948b11922
Shan Chu <shanchu@meta.com>
2025-12-17 11:43:29 -0500

    Initial Commit

---
+++ QRScanPlayUITests/QRScanPlayUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  QRScanPlayUITestsLaunchTests.swift
+//  QRScanPlayUITests
+//
+//  Created by Shan Chu on 12/17/25.
+//
+
+import XCTest
+
+final class QRScanPlayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
